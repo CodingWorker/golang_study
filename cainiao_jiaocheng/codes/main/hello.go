2bcdb36cf6f4b5c2a1d5671f3c70778fba93ecfc
CodingWorker <dy10010@sina.cn>
2017-11-22 22:31:39 +0800

    add note

---
+++ cainiao_jiaocheng/codes/main/hello.go
@@ -0,0 +1,12 @@
+package main
+//import the dependencies
+import "fmt"
+
+var a = "cainaio jiaocheng"
+var b string = "runoob.com"
+var c bool
+
+//main func will be called
+func main(){
+    fmt.Println(a, b, c)
+}
